3e055c1e1fca5a04688569f6ce1dbf40a14ea5ac
Jason <10107412+jason1985@users.noreply.github.com>
2021-07-08 18:23:47 +0700

    add dummy login link

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -29,7 +29,7 @@ export default function Signup() {
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account?
+                Already have an account? <a href="#placeholder">Login</a>
             </div>
         </>
     )
